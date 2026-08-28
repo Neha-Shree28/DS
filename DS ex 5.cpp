0d87ab1b5cb49a5119cb82e78c7e465ee9a292a1
Neha-Shree28 <shreeneha2008@gmail.com>
2026-08-28 13:32:51 +0530

    Refactor infix to postfix conversion code
    Updated the code to improve readability and structure. Added output examples for better understanding.

--- DS ex 5.cpp
+++ DS ex 5.cpp
@@ -95,3 +95,61 @@ printf("The resultant postfix expression is %s\n",postfix);
 getch(); 
 } 
 
+OUTPUT :
+
+1.Insertion,2.Deletion,3.Find,4.Find previous,5.Print,6.Exit
+Enter the choice for operation
+1
+Enter the element to be inserted
+5
+Enter the element after which new to be inserted
+-1
+Before insertion
+NULL
+After insertion
+5->NULL
+
+1.Insertion,2.Deletion,3.Find,4.Find previous,5.Print,6.Exit
+Enter the choice for operation
+1
+Enter the element to be inserted
+9
+Enter the element after which new to be inserted
+5
+Before insertion
+5->NULL
+After insertion
+5->9->NULL
+
+1.Insertion,2.Deletion,3.Find,4.Find previous,5.Print,6.Exit
+Enter the choice for operation
+1
+Enter the element to be inserted
+6
+Enter the element after which new to be inserted
+9
+Before insertion
+5->9->NULL
+After insertion
+5->9->6->NULL
+
+1.Insertion,2.Deletion,3.Find,4.Find previous,5.Print,6.Exit
+Enter the choice for operation
+2
+Enter the element to be deleted
+9
+Before deletion
+5->9->6->NULL
+After deletion
+5->6->NULL
+
+1.Insertion,2.Deletion,3.Find,4.Find previous,5.Print,6.Exit
+Enter the choice for operation
+5
+The elements in the linked list are
+5->6->NULL
+
+1.Insertion,2.Deletion,3.Find,4.Find previous,5.Print,6.Exit
+Enter the choice for operation
+
+
