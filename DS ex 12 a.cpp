f9c322a3493ef9124dd93545dfa5661db339b697
Neha-Shree28 <shreeneha2008@gmail.com>
2026-08-28 14:05:20 +0530

    Add input and output functionality for hashtable

--- DS ex 12 a.cpp
+++ DS ex 12 a.cpp
@@ -35,3 +35,25 @@ for(i=0;i<10;i++)
 printf("%d\t",hashtable[i]);
 getch();
 }
+
+OUTPUT:
+Enter the numbers to be stored in the hashtable, enter -1 to terminate
+enter the number to be entered into the hashtable
+13
+enter the number to be entered into the hashtable
+15
+enter the number to be entered into the hashtable
+16
+enter the number to be entered into the hashtable
+23
+enter the number to be entered into the hashtable
+14
+enter the number to be entered into the hashtable
+24
+enter the number to be entered into the hashtable
+
+89
+
+-1
+The elements of the hashtable are
+0 0 0 13 23 15 16 14 24 0
